19bba97154d928f26a1c5a1d5fcac3076f5dd171
blueicy <aowlee@gmail.com>
2020-07-27 18:42:17 +0900

    inapp purchase consumable

---
+++ 4week/ Foodzilla/Foodzilla/Helpers/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Foodzilla
+//
+//  Created by Caleb Stultz on 9/14/17.
+//  Copyright © 2017 Caleb Stultz. All rights reserved.
+//
+
+import Foundation
+
+// MARK: – IAP Identifiers
+let IAP_MEAL_ID = "com.iapcourse.meal"
+let IAP_HIDE_ADS_ID = "com.iapcourse.hideadvertisements"
+
+// MARK: – Notification Identifiers
+let IAPServicePurchaseNotification = "IAPServicePurchaseNotification"
+let IAPServiceRestoreNotification = "IAPServiceRestoreNotification"
+let IAPServiceFailureNotification = "IAPServiceFailureNotification"
