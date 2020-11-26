6f1f52f8afe4ad5b31e9dfd46e0f5bc11f154bc0
blueicy <aowlee@gmail.com>
2020-11-26 18:34:48 +0900

    ADD    leet findSubstring

--- structure/leet/leet/main.swift
+++ structure/leet/leet/main.swift
@@ -597,36 +597,93 @@ public class ListNode {
 //}
 
 
+//class Solution {
+//    func strStr(_ haystack: String, _ needle: String) -> Int {
+//        if haystack.isEmpty {
+//            if needle.isEmpty { return 0 }
+//            return -1
+//        }
+//
+//        if needle.isEmpty { return 0 }
+//
+//        let needleCount = needle.count
+//        var start = 0
+//        var end = needleCount - 1
+//
+//        while end < haystack.count {
+//            let startIndex = haystack.index(haystack.startIndex, offsetBy:start)
+//            let endIndex = haystack.index(haystack.startIndex, offsetBy:end)
+//            let substring = haystack[startIndex...endIndex]
+//
+//            if substring == needle {
+//                return start
+//            }
+//
+//            start += 1
+//            end += 1
+//
+//        }
+//
+//        return -1
+//
+//
+//
+//    }
+//}
+
+
 class Solution {
-    func strStr(_ haystack: String, _ needle: String) -> Int {
-        if haystack.isEmpty {
-            if needle.isEmpty { return 0 }
-            return -1
-        }
+    class TrieNode {
+        var word: String?
+        var children = [Character: TrieNode]()
+    }
+
+    func findSubstring(_ s: String, _ words: [String]) -> [Int] {
+        guard words.count > 0 && words[0].count > 0 else { return [] }
+        let count = words.count, len = words[0].count
         
-        if needle.isEmpty { return 0 }
-        
-        let needleCount = needle.count
-        var start = 0
-        var end = needleCount - 1
-        
-        while end < haystack.count {
-            let startIndex = haystack.index(haystack.startIndex, offsetBy:start)
-            let endIndex = haystack.index(haystack.startIndex, offsetBy:end)
-            let substring = haystack[startIndex...endIndex]
-            
-            if substring == needle {
-                return start
+        // build trie
+        let trieRoot = TrieNode()
+        for word in words {
+            var node = trieRoot
+            for char in word {
+                if node.children[char] == nil {
+                    node.children[char] = TrieNode()
+                }
+                node = node.children[char]!
             }
-            
-            start += 1
-            end += 1
-            
+            node.word = word
         }
         
-        return -1
+        // build counter
+        var counter = [String: Int]()
+        for word in words {
+            counter[word, default: 0] += 1
+        }
         
+        // search
+        var result = [Int]()
+        let s = Array(s)
+        iLoop: for i in 0..<s.count where i + len * count <= s.count {
+            var j = i
+            var counter = counter
+            while j < i + len * count {
+                var node = trieRoot, k = j
+                while k < j + len {
+                    guard let next = node.children[s[k]] else { continue iLoop }
+                    node = next
+                    k += 1
+                }
+                
+                let word = node.word!
+                guard counter[word, default: 0] > 0 else { continue iLoop }
+                counter[word, default: 0] -= 1
+                
+                j = k
+            }
+            result.append(i)
+        }
         
-        
+        return result
     }
 }
