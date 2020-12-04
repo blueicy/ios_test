7501cb52e9fa0049114559df59ae98557fddf725
blueicy <aowlee@gmail.com>
2020-12-04 20:49:55 +0900

    ADD    leet combinationSum

--- structure/leet/leet/main.swift
+++ structure/leet/leet/main.swift
@@ -1059,3 +1059,30 @@ public class ListNode {
 //
 //    }
 //}
+
+class Solution {
+    func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        var result = [[Int]]()
+        var candidate = [Int]()
+        
+        backtracking(&result, &candidate, candidates.sorted(), target, 0)
+        
+        return result
+    }
+    
+    private func backtracking(_ result: inout [[Int]], _ candidate: inout [Int], _ candidates:[Int], _ remain:Int, _ start:Int) {
+        if remain < 0 {
+            return
+        } else if remain == 0 {
+            result.append(candidate)
+        } else {
+            for i in start..<candidates.count {
+                candidate.append(candidates[i])
+                backtracking(&result, &candidate, candidates, remain - candidates[i], i)
+                candidate.removeLast()
+            }
+        }
+    }
+    
+    
+}
