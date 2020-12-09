6fb35049721d6ba051bfa932e1f22e5e1cd42629
blueicy <aowlee@gmail.com>
2020-12-09 20:39:22 +0900

    ADD    leet

--- structure/leet/leet/main.swift
+++ structure/leet/leet/main.swift
@@ -1197,28 +1197,28 @@ public class ListNode {
 // 
 //}
 
-//https://leetcode.com/problems/multiply-strings/discuss/562036/Swift-Explanation-Detailed-step-by-step
-class Solution {
-    func multiply(_ num1: String, _ num2: String) -> String {
-        var num1A = Array(num1)
-        var num2A = Array(num2)
-        var sum = [Int](repeating: 0, count: num1.count + num2.count)
-        for (i,n1) in num1A.enumerated().reversed() {
-            for (j, n2) in num2A.enumerated().reversed() {
-                guard let digit1 = n2.wholeNumberValue, let digit2 = n1.wholeNumberValue else { return ""}
-                
-                let p1 = i + j
-                let p2 = i + j + 1
-                let val = digit1 * digit2 + sum[p2]
-                sum[p2] = val % 10
-                sum[p1] += val / 10
-                
-            }
-        }
-        while let firstSum = sum.first, firstSum == 0 { sum.removeFirst() }
-        let res = sum.map ({ String($0)}).joined()
-        return res.count > 0 ? res : "0"
-        
-        
-    }
-}
+////https://leetcode.com/problems/multiply-strings/discuss/562036/Swift-Explanation-Detailed-step-by-step
+//class Solution {
+//    func multiply(_ num1: String, _ num2: String) -> String {
+//        var num1A = Array(num1)
+//        var num2A = Array(num2)
+//        var sum = [Int](repeating: 0, count: num1.count + num2.count)
+//        for (i,n1) in num1A.enumerated().reversed() {
+//            for (j, n2) in num2A.enumerated().reversed() {
+//                guard let digit1 = n2.wholeNumberValue, let digit2 = n1.wholeNumberValue else { return ""}
+//                
+//                let p1 = i + j
+//                let p2 = i + j + 1
+//                let val = digit1 * digit2 + sum[p2]
+//                sum[p2] = val % 10
+//                sum[p1] += val / 10
+//                
+//            }
+//        }
+//        while let firstSum = sum.first, firstSum == 0 { sum.removeFirst() }
+//        let res = sum.map ({ String($0)}).joined()
+//        return res.count > 0 ? res : "0"
+//        
+//        
+//    }
+//}
