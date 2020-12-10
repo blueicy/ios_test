1979133f4a7be68aa54a6375f4e21111f796358a
blueicy <aowlee@gmail.com>
2020-12-10 20:31:50 +0900

    ADD    leet

--- structure/leet/leet/main.swift
+++ structure/leet/leet/main.swift
@@ -1206,19 +1206,19 @@ public class ListNode {
 //        for (i,n1) in num1A.enumerated().reversed() {
 //            for (j, n2) in num2A.enumerated().reversed() {
 //                guard let digit1 = n2.wholeNumberValue, let digit2 = n1.wholeNumberValue else { return ""}
-//                
+//
 //                let p1 = i + j
 //                let p2 = i + j + 1
 //                let val = digit1 * digit2 + sum[p2]
 //                sum[p2] = val % 10
 //                sum[p1] += val / 10
-//                
+//
 //            }
 //        }
 //        while let firstSum = sum.first, firstSum == 0 { sum.removeFirst() }
 //        let res = sum.map ({ String($0)}).joined()
 //        return res.count > 0 ? res : "0"
-//        
-//        
+//
+//
 //    }
 //}
