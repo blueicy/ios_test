97a1a57dd1bb67f3705813f207ee78ad7e621483
blueicy <aowlee@gmail.com>
2020-12-17 20:55:51 +0900

    ADD    matrix

--- structure/leet/leet/main.swift
+++ structure/leet/leet/main.swift
@@ -1269,42 +1269,56 @@ public class ListNode {
 //}
 
 
+//class Solution {
+//    func permuteUnique(_ nums: [Int]) -> [[Int]] {
+//        if nums.count == 0 {
+//            return []
+//        }
+//
+//        let nums = nums.sorted()
+//        var result = [[Int]]()
+//        var candidate = [Int]()
+//        var visited = [Bool](repeatElement(false, count: nums.count))
+//
+//        backtracking(&result, &candidate, nums, &visited)
+//
+//        return result
+//    }
+//
+//    func backtracking(_ result: inout [[Int]], _ candidate: inout [Int], _ nums: [Int], _ visited: inout [Bool]) {
+//        if candidate.count == nums.count {
+//            result.append(candidate)
+//            return
+//        }
+//
+//        for i in 0..<nums.count {
+//            if visited[i] || ( i>0 && !visited[i - 1] && nums[i - 1] == nums[i]) {
+//                continue
+//            }
+//
+//            visited[i] = true
+//            candidate.append(nums[i])
+//            backtracking(&result, &candidate, nums, &visited)
+//            visited[i] = false
+//            candidate.removeLast()
+//
+//
+//        }
+//
+//    }
+//
+//}
+
 class Solution {
-    func permuteUnique(_ nums: [Int]) -> [[Int]] {
-        if nums.count == 0 {
-            return []
-        }
-        
-        let nums = nums.sorted()
-        var result = [[Int]]()
-        var candidate = [Int]()
-        var visited = [Bool](repeatElement(false, count: nums.count))
-        
-        backtracking(&result, &candidate, nums, &visited)
-        
-        return result
-    }
-    
-    func backtracking(_ result: inout [[Int]], _ candidate: inout [Int], _ nums: [Int], _ visited: inout [Bool]) {
-        if candidate.count == nums.count {
-            result.append(candidate)
-            return
-        }
-        
-        for i in 0..<nums.count {
-            if visited[i] || ( i>0 && !visited[i - 1] && nums[i - 1] == nums[i]) {
-                continue
+    func rotate(_ matrix: inout [[Int]]) {
+        for i in 0..<matrix.count {
+            for tj in i+1..<matrix[i].count {
+                let t = matrix[i][j]
+                matrix[i][j] = matrix[j][i]
+                matrix[j][i] = t
             }
             
-            visited[i] = true
-            candidate.append(nums[i])
-            backtracking(&result, &candidate, nums, &visited)
-            visited[i] = false
-            candidate.removeLast()
-            
-
+            matrix[i].reverse()
         }
-
     }
-    
 }
