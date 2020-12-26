9fe6faec0b58c6c2de802260f4adb0e1ae4dd764
blueicy <aowlee@gmail.com>
2020-12-26 19:01:40 +0900

    ADD    leet merge

--- structure/leet/leet/main.swift
+++ structure/leet/leet/main.swift
@@ -1591,17 +1591,52 @@ public class ListNode {
 //}
 
 
+//class Solution {
+//    func canJump(_ nums: [Int]) -> Bool {
+//        let count = nums.count
+//
+//        var maxJump = 0
+//        for i in nums.indices {
+//            if i <= maxJump {
+//                maxJump = max(maxJump, i + nums[i])
+//            }
+//        }
+//
+//        return maxJump >= nums.count - 1
+//    }
+//}
+
+
 class Solution {
-    func canJump(_ nums: [Int]) -> Bool {
-        let count = nums.count
+    func merge(_ intervals: [[Int]]) -> [[Int]] {
+        if intervals.count < 1 {
+            return intervals
+        }
+        var intervals = intervals.sorted{$0[0] < $1[0]}
+        var results = [intervals[0]]
         
-        var maxJump = 0
-        for i in nums.indices {
-            if i <= maxJump {
-                maxJump = max(maxJump, i + nums[i])
+        for i in 1..<intervals.count {
+            let interval = intervals[i]
+            let lastIndex = results.count - 1
+            
+            if overlap(results[lastIndex], interval){
+                results[lastIndex] = [min(results[lastIndex][0], interval[0]), max(results[lastIndex][1], interval[1])]
+            } else {
+                results.append(interval)
             }
+            
         }
         
-        return maxJump >= nums.count - 1
+        return results
+        
+        
     }
+    
+    
+    func overlap(_ a:[Int], _ b: [Int]) -> Bool {
+        return !(b[0] > a[1])
+    }
+    
+    
+    
 }
